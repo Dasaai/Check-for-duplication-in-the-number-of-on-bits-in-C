d5f917ca2ef4b2f86d50afb90a249e0c3ce6bb28
Dasaai <73518606+Dasaai@users.noreply.github.com>
2020-10-28 15:06:10 +0700

    Check.cpp

--- Check.cpp
+++ Check.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <clocale>
 #include <conio.h>
 
 int main(){
-    setlocale(0, "rus");
     int a, b, n;
-    printf("Введите a\n");
+    printf("Input a\n");
     scanf_s("%d", &a);
     if (a >= 10 || a <= -10) {
         while (a != 0) {
@@ -15,14 +13,14 @@ int main(){
                 if (n != b % 10)
                     b = b / 10;
                 else {
-                    printf("Есть\n");
+                    printf("yes\n");
                     return 0;
                 }
             }
             a = a / 10;
         }
-        printf("нету");
+        printf("no");
     }
-    else printf("Ошибка ввода");
+    else printf("error");
     return 0;
 }
